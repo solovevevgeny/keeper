b750e72c2daf6a1e14f5177373ba9e5de4e77a9f
solovevevgeny <33507596+solovevevgeny@users.noreply.github.com>
2020-08-31 13:11:31 +0300

    creating model, controller, migration: Operations

---
+++ database/seeds/OperationsSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class OperationsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
