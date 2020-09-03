f4716839fe4f7785ea9d12adb917b9e6c6138663
Solovev Evgeny <evgeny.solovev@gmail.com>
2020-09-03 18:06:24 +0300

    css beauty

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,10 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
 <!-- /BOOTSTRAP IO -->
+<link rel="stylesheet" href="/css/main.css">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;400;500&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
 
 </head>
 
--- resources/views/mainscreen/index.blade.php
+++ resources/views/mainscreen/index.blade.php
@@ -9,7 +9,6 @@
             @foreach($accounts as $account)
 
             <div class="card" style="width: 14rem; float:left;">
-            <div style="background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);">&nbsp;</div>
             <div class="card-body">
                 <h5 class="card-title">{{ $account->name}}</h5>
                 <p class="card-text">{{ number_format($account->amount,2,'.', ' ') }} {{ $account->currency->sufix }}  </p>
@@ -22,17 +21,32 @@
     <div style="width:100%; height:1px; clear:both;"></div>
     <div class="operations">
         <h3>Операции</h3>    
+        <ul class="list-group">
         @if($operations !== null)
             @foreach($operations as $operation)
-                @if ($operation->accountFrom !== null)
-                    <div style='float:left'> {{ $operation->accountFrom->name }}</div>
-                @endif
-                <div style='float:right'>{{ number_format($operation->amount, 2, '.', ' ') }}</div><br />
-            @endforeach
-        @endif
+                    @if ($operation->accountFrom !== null)
+                        <li class="list-group-item">{{ $operation->accountFrom->name }} 
 
+                        @if ($operation->category !== null)
+                        <i class="fas fa-arrow-right"></i> {{ $operation->category->title }} 
+                        @endif
+
+                        @if ($operation->accountTo !== null)
+                        <i class="fas fa-arrow-right"></i> {{ $operation->accountTo->name }} 
+                        @endif
+
+
+                        <div style="float:right">{{ number_format($operation->amount, 2, '.', ' ') }}</div> 
+                        <div class="bg-light">{{ $operation->comment }}</div>
+                        </li>
+                    @endif
+            @endforeach
+        </ul>
+        @endif
+    </div>
 
     <div class="operationsGraph">
+        <h3><i class="fas fa-chart-line"></i> Статистика</h3>
         @include("chars.sample")
     </div>
 
