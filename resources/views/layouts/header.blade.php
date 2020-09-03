0bd71c19c129a385775555c253943a5cec0671db
solovevevgeny <33507596+solovevevgeny@users.noreply.github.com>
2020-09-03 15:37:38 +0300

    Translate header menu

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,5 +1,5 @@
-<a href="{{ route('mainscreen.index') }}">Home</a>
+<a href="{{ route('mainscreen.index') }}">Главная</a>
 
-<a href="{{ route('operations.index') }}">Operations</a>
+<a href="{{ route('operations.index') }}">Операции</a>
 [ <a href="{{ route('operations.createForm') }}">+</a> ]
-<a href="{{ route('accounts.index') }}">Accounts</a>
+<a href="{{ route('accounts.index') }}">Счета</a>
