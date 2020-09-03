d587dba5fa681603f1c5f0978771ce934d927bd9
solovevevgeny <33507596+solovevevgeny@users.noreply.github.com>
2020-09-03 11:43:50 +0300

    spaces

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ route::get("/operations", "OperationController@index")->name("operations.index")
 route::get("/operations/create","OperationController@createForm")->name("operations.createForm");
 route::post("/operations/create","OperationController@store")->name("operations.store");
 
-
 route::get("/accounts", "AccountController@index")->name("accounts.index");
-
 
 Route::get('/', "MainScreenController@index")->name("mainscreen.index");
